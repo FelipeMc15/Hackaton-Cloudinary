b7729070ca383a19800f414f07429eed8c77c650
Felipe Morales <carrionfelipe15@gmail.com>
2023-02-21 20:13:13 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h1 className=" text-sky-400 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p className="bg-sky-400">The quick brown fox...</p>
+    </div>
+  );
+}
+
+export default App;
